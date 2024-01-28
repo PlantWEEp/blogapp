6974684a0da3b8b2a55d11ee81329b139bd1cfb8
Vaishnav <vaishnav0931@gmail.com>
2024-01-28 20:23:40 +0530

    project get started ✏️🚀

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react' 
+import './App.css'
+import Login from './components/authfrom/Login'
+import Signup from './components/authfrom/Signup'
+import { Route, Routes, useLocation } from 'react-router-dom'
+
+function App() { 
+
+  return (
+    <>
+     <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Signup/>}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      // Customizing font sizes
+      fontSize: {
+        "heading-lg": "64px",    // Large heading size
+        "custom-h1": "64px",     // Custom H1 size
+        "custom-h2": "56px",     // Custom H2 size
+        "custom-h3": "48px",     // Custom H3 size
+        "custom-h4": "32px",     // Custom H4 size
+        "custom-h5": "24px",     // Custom H5 size
+        "custom-h6": "20px",     // Custom H6 size
+        "custom-p": "14px",      // Custom paragraph size
+        "links-p": "18px",       // Font size for links in paragraphs
+      },
+      // Customizing text colors
+      textColor: {
+        skin: {
+          textblack: "var( --background-color-base)",
+          textwhite: "var(--color-white)",
+        },
+      },
+      // Customizing background colors
+      backgroundColor: {
+        skin: {
+          base: "var(--background-color-base)", // Base background color
+          hover: "var(--hover-color)",          // Background color on hover
+          greybg: "var(--base-bg-color)",       // Grey background color
+        },
+      },
+    },
+    // Defining custom screen breakpoints
+    screens: {
+      "2xl": { max: "1400px" }, // Custom 2xl screen size
+      xl: { max: "1279px" },     // Extra large screen size
+      lg: { max: "999px" },      // Large screen size
+      md: { max: "767px" },      // Medium screen size
+      sm: { max: "639px" },      // Small screen size
+      ssm: { max: "499px" },     // Extra small screen size
+    },
+  },
+  plugins: [],
+};
