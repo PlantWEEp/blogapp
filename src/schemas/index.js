7d42408c16f141c8e7cf6f825638d2877f3ae055
Vaishnav <vaishnav0931@gmail.com>
2024-02-03 12:24:11 +0530

    opt update

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,20 +3,48 @@ import * as yup from "yup";
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 // min 8 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
 
-export const BasicSchema = yup.object().shape({
+ const BasicSchema = yup.object().shape({
   email: yup.string().email("Please enter a valid email").required("Email-id is Required "),
   
   password: yup
     .string()
-<<<<<<< HEAD
     .min(8)
     .matches(passwordRules, { message: "1 upper case letter, 1 lower case letter, 1 numeric digit" })
     .required("Password is Required "), 
-=======
-    .min(5)
-    .matches(passwordRules, { message: "Please create a stronger password" })
-    .required("1 upper case letter, 1 lower case letter, 1 numeric digit "),
- 
->>>>>>> d4edc0ff1d0a9c34831bbf5aa09b51fe6842eff7
 });
 
+const otpSchema = yup.object().shape({
+  digitoneOne: yup
+    .string()
+    .matches(/^\d$/, "OTP should be a single digit")
+    .required("Please enter OTP digit"),
+
+  digitoneTwo: yup
+    .string()
+    .matches(/^\d$/, "OTP should be a single digit")
+    .required("Please enter OTP digit"),
+
+  digitonThree: yup
+    .string()
+    .matches(/^\d$/, "OTP should be a single digit")
+    .required("Please enter OTP digit"),
+
+  digitoneFour: yup
+    .string()
+    .matches(/^\d$/, "OTP should be a single digit")
+    .required("Please enter OTP digit"),
+
+  digitoneFive: yup
+    .string()
+    .matches(/^\d$/, "OTP should be a single digit")
+    .required("Please enter OTP digit"),
+
+  digitoneSix: yup
+    .string()
+    .matches(/^\d$/, "OTP should be a single digit")
+    .required("Please enter OTP digit")
+});
+
+
+
+export {BasicSchema,otpSchema}
