e65e4842c5007ec0727052a7e5d6181b18120c30
SonuRamesh <130997188+SonuRamesh@users.noreply.github.com>
2024-02-10 11:54:21 +0530

    update (connected login to firebase)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"; 
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.VITE_API_KEY,
-    authDomain: process.env.VITE_AUTH_DOMIAN,
-    projectId: process.env.VITE_PROJECT_ID,
-    storageBucket: process.env.VITE_STORAGE_BUCKET,
-    messagingSenderId:process.env.VITE_MESSAGING_SENDER_ID,
-    appId: process.env.VITE_APP_ID,
-    measurementId: process.env.VITE_MEASUREMENT_ID
+    apiKey: "AIzaSyBwuIqBIELL1wCc1vzK4EQHgFb5OrFkEws",
+    authDomain: "waveapp-c671c.firebaseapp.com",
+    projectId: "waveapp-c671c",
+    storageBucket: "waveapp-c671c.appspot.com",
+    messagingSenderId: "78276349617",
+    appId: "1:78276349617:web:16fb066db7a3220a331c90",
+    measurementId: "G-RZDM639RY3",
 }; 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); 
+const firestore = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth };
+export { app, auth, firestore, storage };
